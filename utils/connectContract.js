3f21c2084a07c7e2b3bef5cbedfd2effa752d043
Marybeee <maria.behappy.mb@gmail.com>
2022-08-11 19:24:03 +0200

    added contract address

--- utils/connectContract.js
+++ utils/connectContract.js
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 
 function connectContract() {
     //Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
-    const contractAddress = "0x[YOUR_CONTRACT_ADDRESS]";
+    const contractAddress = "0xCe4Bb8eAb4523b84dA1762EBD7e78F0053a1883a";
     const contractABI = abiJSON.abi;
     let rsvpContract;
     try {
